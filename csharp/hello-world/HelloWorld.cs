1968d75692302e39105ef6e589f34318288920d3
Michel Balthazar <michel@nfe.io>
2017-03-01 19:24:20 -0300

    created the repository; fetched hello world csharp

---
+++ csharp/hello-world/HelloWorld.cs
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+  public static string Hello()
+  {
+    return null;
+    }
+
+  public static string Hello(string name)
+  {
+    return null;
+  }
+}
---
+++ csharp/hello-world/HelloWorldTest.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class HelloWorldTest
+{
+    [Test]
+    public void Say_hi()
+    {
+        Assert.That(HelloWorld.Hello(), Is.EqualTo("Hello, World!"));
+    }
+}
