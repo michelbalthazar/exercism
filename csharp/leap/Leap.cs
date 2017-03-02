e39b147c0737429be7812a763a6272304456e4bb
Michel Balthazar <michel@nfe.io>
2017-03-02 14:25:58 -0300

    Refactor test

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -2,12 +2,6 @@ public class Year
 {
     public static bool IsLeap(int year)
     {
-        if (year % 4 == 0 && year % 100 > 0 && year % 400 > 0)
-            return true;
-        
-        else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
-            return true;
-            
-        return false;
+        return (year % 4 == 0 && year % 100 > 0 || year % 400 == 0);
     }  
 }
