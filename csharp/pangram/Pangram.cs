484edeaa2722d6a3524b21cc2b38404cd5afb657
Michel Balthazar <michel@nfe.io>
2017-03-03 20:46:29 -0300

    create the project and first test.

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,11 @@
+public class Pangram
+{
+    public static bool IsPangram(string pangram)
+    {
+        if(pangram == "")
+            return false;
+        
+        return false;
+
+    }           
+}
