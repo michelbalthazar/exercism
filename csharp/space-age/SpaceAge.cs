d38aa2025415d16d0112c330288963eb379de14b
Michel Balthazar <michel@nfe.io>
2017-03-03 15:16:21 -0300

    implemented all the tests

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -2,16 +2,16 @@ using System;
 
 public class SpaceAge
 {
-    private int _second;
+    private double _second;
 
-    public SpaceAge(int second)
+    public SpaceAge(double second)
     {
         this._second = second;
     }
 
-    public int Seconds()
+    public double Seconds()
     {
-        int age = _second;
+        double age = _second;
         return age;
     }
 
@@ -19,7 +19,55 @@ public class SpaceAge
     {
         double age = 31557600;
         age = _second / age;
+        return age;
+    }
 
+    public double OnMercury()
+    {
+        double age = 7600543.81992;
+        age = _second / age;
+        return age;
+    }
+
+    public double OnVenus()
+    {
+        double age = 19414149.052176;
+        age = _second / age;
+        return age;
+    }
+
+    public double OnMars()
+    {
+        double age = 59354032.69008;
+        age = _second / age;
+        return age;
+    }
+
+    public double OnJupiter()
+    {
+        double age = 374355659.124;
+        age = _second / age;
+        return age;
+    }
+
+    public double OnSaturn()
+    {
+        double age = 929292362.8848;
+        age = _second / age;
+        return age;
+    }
+
+    public double OnUranus()
+    {
+        double age = 2651370019.3296;
+        age = _second / age;
+        return age;
+    }
+
+    public double OnNeptune()
+    {
+        double age = 5200418560.032;
+        age = _second / age;
         return age;
     }
 }
