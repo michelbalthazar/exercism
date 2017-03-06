fda6aab160f7ae4c45a4ddd91cc69815a05e220f
Michel Balthazar <michel@nfe.io>
2017-03-06 16:46:33 -0300

    Identention

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -1,34 +1,30 @@
 using System;
 using System.Collections.Generic;
-
 public class DNA
 {
     private string _nucleo;
-    public DNA(string nucleo)
+    public DNA (string nucleo)
     {
        this._nucleo = nucleo;
     }
   
-    public Dictionary<char, int> NucleotideCounts()
+    public Dictionary<char, int> NucleotideCounts ()
     {
-        
         return new Dictionary<char, int> { { 'A', Count('A') }, { 'T', Count('T')} , { 'C', Count('C') }, { 'G', Count('G') } };       
     }
 
-    internal int Count(char v)
+    internal int Count (char v)
     {      
-        
         int count = 0;
-        foreach(var validateDna in _nucleo)
+        foreach (var validateDna in _nucleo)
         {
-            if(validateDna == v)
+            if (validateDna == v)
                 count++;
-                
-           if(v != 'A' && v != 'C' && v != 'T' && v != 'G')  
+
+            if (v != 'A' && v != 'C' && v != 'T' && v != 'G')  
              throw new InvalidNucleotideException();
         }
-        
-               
+
         return count;
     }
 }
