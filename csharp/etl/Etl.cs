1e4fd208af3640090fbec4b243964cf6f5600fbb
Michel Balthazar <michel@nfe.io>
2017-03-07 18:06:04 -0300

    create project

---
+++ csharp/etl/Etl.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+public class ETL
+{
+    public Dictionary<int, List<string>> _etl = new Dictionary<int, List<string>> ();
+
+    internal static object Transform(Dictionary<int, IList<string>> old)
+    {
+        return 0;
+    }
+}
