9dedaae8b3191fbdbea05dfea4c5ea6a0f8ac4a3
Michel Balthazar <michel@nfe.io>
2017-03-07 19:17:22 -0300

    Create initial methods

--- csharp/etl/Etl.cs
+++ csharp/etl/Etl.cs
@@ -4,9 +4,26 @@ using System.Collections.Generic;
 public class ETL
 {
     public Dictionary<int, List<string>> _etl = new Dictionary<int, List<string>> ();
+    public void AddList (int key, string value)
+    {
+        if(this._etl.ContainsKey (key))
+        {
+            List<string> list = this._etl [key];
+            if (list.Contains (value) == false)
+                list.Add (value);
+        }
+        else
+        {
+            List<string> list = new List<string>();
+            list.Add(value);
+            this._etl.Add (key, list);
+        }
+    }
 
     internal static object Transform(Dictionary<int, IList<string>> old)
     {
-        return 0;
+        
+       
+        return old;
     }
 }
