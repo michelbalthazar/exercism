ba303901f40ee2fa2b6e11ceaaf194d65ca9f0a1
Michel Balthazar <michel@nfe.io>
2017-03-09 12:49:47 -0300

    Implement Score Method

--- csharp/scrabble-score/ScrabbleScore.cs
+++ csharp/scrabble-score/ScrabbleScore.cs
@@ -2,8 +2,30 @@ using System;
 
 public static class Scrabble
 {
-    public static int Score(string input)
+    public static int Score(string inputs)
     {
-        return "";
+        int num = 0;
+        input.ToUpper();
+        foreach (var input in inputs)
+        {
+       
+        if (input == "A" || input == "A" || input == "A" || input == "A" || input == "A"
+            || input == "A" || input == "A" || input == "A" || input == "A" || input == "A")
+                num += 1;
+            
+            else if( input == "G" || input == "D")
+                num += 2;
+                else if(input == "B" || input == "C" || input == "M" || input == "P" || )
+                    num += 3;
+                    else if(input == "F" || input == "H" || input == "V" || input == "W" || input == "Y" ||)
+                        num += 4;
+                        else if(input == "K")
+                            num += 5;
+                            else if(input == "J" || input == "X" ||)
+                                num += 8;
+                                else if(input == "Q" || input == "Z" ||)
+                                    num += 10;
+        }             
+        return num;
     }
 }
