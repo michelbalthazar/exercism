eb4b61d28cd834c9e49bd1cddca490a0c6cab1fd
Michel Balthazar <michel@nfe.io>
2017-03-09 21:06:37 -0300

    create project Error-Handling and implement two methods

---
+++ csharp/error-handling/ErrorHandling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+         throw new Exception();
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        int successfulResult = 0;
+        if (input == "a")
+        {
+        return null;
+        }
+         return successfulResult = 1;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
