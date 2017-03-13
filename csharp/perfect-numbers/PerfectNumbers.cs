8edf28c8d46424dd46a738151cb635472747fd03
Michel Balthazar <michel@nfe.io>
2017-03-13 10:36:12 -0300

    create projec t

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public enum NumberType
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static NumberType Classify(int number)
+    {
+       NumberType result;
+       int sum = 0;
+       for(int i = 0; i <= number; i++)
+       {
+           if(number % i == 0)
+           {
+                sum += 1;
+           }
+       }
+       if(sum == number)
+        return Enum.GetNames.GetType.Perfect;
+
+        return result;
+    }
+}
---
+++ csharp/perfect-numbers/PerfectNumbersTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+public class PerfectNumbersTest
+{
+    [Theory]
+    [InlineData(3)]
+    [InlineData(7)]
+    [InlineData(13)]
+    public void Can_classify_deficient_numbers(int number)
+    {
+        Assert.Equal(NumberType.Deficient, PerfectNumbers.Classify(number));
+    }
+    
+    [Theory(Skip = "Remove to run test")]
+    [InlineData(6)]
+    [InlineData(28)]
+    [InlineData(496)]
+    public void Can_classify_perfect_numbers(int number)
+    {
+        Assert.Equal(NumberType.Perfect, PerfectNumbers.Classify(number));
+    }
+    
+    [Theory(Skip = "Remove to run test")]
+    [InlineData(12)]
+    [InlineData(18)]
+    [InlineData(20)]
+    public void Can_classify_abundant_numbers(int number)
+    {
+        Assert.Equal(NumberType.Abundant, PerfectNumbers.Classify(number));
+    }
+}
