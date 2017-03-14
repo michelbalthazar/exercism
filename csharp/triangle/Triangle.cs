f901ecb34a0e32daa79806c8a0e96856b6667056
Michel Balthazar <michel@nfe.io>
2017-03-14 17:06:04 -0300

    Implement TriangleKind and Validate Method

--- csharp/triangle/Triangle.cs
+++ csharp/triangle/Triangle.cs
@@ -11,7 +11,27 @@ public static class Triangle
 {
     public static TriangleKind Kind(decimal side1, decimal side2, decimal side3)
     {
+        if (!Validate(side1, side2, side3))
+            throw new TriangleException();
+        if( side1 == side2 && side2 == side3)
+        {
+            return TriangleKind.Equilateral;    
+        }
+        if(side1 != side2 && side2 != side3 && side1 != side3)
+        {
+            return TriangleKind.Scalene;
+        }
+
         return TriangleKind.Isosceles;
+    }
+    private static bool Validate(decimal a, decimal b, decimal c)
+    {
+        if (a < 0 || b < 0 || c < 0)
+            return false;
+        if (a < b + c && b < a + c && c < a + b)
+            return true;
+
+        return false;
     }
 }
 
