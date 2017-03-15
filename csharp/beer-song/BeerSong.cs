ff90ba45c1ce518d004828171186405af460d436
Michel Balthazar <michel@nfe.io>
2017-03-15 17:27:04 -0300

    Create project, implement Verse and Sing methods.

--- csharp/beer-song/BeerSong.cs
+++ csharp/beer-song/BeerSong.cs
@@ -2,37 +2,51 @@
 
 public static class Beer
 {
+    private static string[] _song = new string[] 
+            {
+                "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n",
+
+                "7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n",
+
+                "6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n",
+
+                "5 bottles of beer on the wall, 5 bottles of beer.\nTake one down and pass it around, 4 bottles of beer on the wall.\n",
+
+                "4 bottles of beer on the wall, 4 bottles of beer.\nTake one down and pass it around, 3 bottles of beer on the wall.\n",
+
+                "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n",
+
+                "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n",
+
+                "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
+
+                "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+            };
     public static string Verse(int number)
     {
-        int numberTo = number;
-        numberTo -= 1;
-        
-            string song1, song2;
-            // song1 = number.ToString() + " bottles of beer on the wall, " + number.ToString() 
-            // + " bottles of beer.\nTake one down and pass it around, ";
-            // song2 = numberTo.ToString() + "bottles of beer on the wall.\n";
-            
-            song1 = number.ToString() + " bottles of beer on the wall, " + 
-            number.ToString() + " bottles of beer.\nTake one down and pass it around, " + 
-            numberTo.ToString() + " bottles of beer on the wall.\n";
 
-            // if(number == 0)
-            // {
-            //     song1.Replace("*", number1);
-            //     song2.Replace("/", number2);
-            //     song1 = "* bottles of beer on the wall, * bottles of beer.\nTake one down and pass it around, / bottles of beer on the wall.\n";
-            //     song2 = "/ bottles of beer on the wall.\n";
-            //     number = 99;
-            // }
+        foreach(var item in _song)
+        {
+            if(item.StartsWith(number.ToString()))
+            {
+                return item;
+            }
 
-    string teste = "testandoS";
-    teste.Replace("test", "seils");
-            Console.WriteLine("TESTEEEE:  "+ teste.Replace("T", "MM") +"  "+ numberTo);
-            return song1;
+        }
+        return _song[_song.Length - 1];
     }
 
     public static string Sing(int start, int stop)
     {
-        throw new NotImplementedException("You need to implement this function.");
+         foreach(var item in _song)
+            {
+                if(item.Contains(start.ToString()) && item.Contains(start.ToString()))
+                {
+                    for(int i = start; i <= stop; i++)
+                        return item;
+                }
+
+            }
+            return _song[_song.Length - 1];
     }
 }
